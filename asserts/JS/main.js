32900b66054bf3fffd9cca7a89bf0b2626eaeea7
Navaharish <navaharish2005@gmail.com>
2023-06-02 09:42:24 +0530

    complete one page webpage is ready now

--- asserts/JS/main.js
+++ asserts/JS/main.js
@@ -1,16 +1,76 @@
-//setting toggle menu
-let navToggler = document.querySelector('.nav-toggler');
+const navToggler = document.querySelector(".nav-toggler");
+const navLinks = document.querySelectorAll(".header-menu>li>a");
+const navLinkBtn = document.querySelector(".header-menu>li>.btn-primary");
 
-navToggler.addEventListener('click', () => {
-    let headerMenu = document.querySelector('ul.header-menu');
-    let navIcon = document.querySelectorAll('span.nav-icon');
+console.log(navLinkBtn);
+navToggler.addEventListener("click", changeMenuState);
 
+function changeMenuState() {
+    const headerMenu = document.querySelector("ul.header-menu");
+    const navIcon = document.querySelectorAll(".navIcon");
 
-    headerMenu.classList.toggle('slide');
+    // Show menu programatically
+    headerMenu.classList.toggle("slide");
 
-    navIcon.forEach(icon => {
-        icon.classList.toggle('hidden')
-    })
-})
+    navIcon.forEach((icon) => {
+        icon.classList.toggle("hidden");
+    });
+}
 
-//setting active menu
+function navbarFixed() {
+    const header_dom = document.querySelector(".header");
+    const nav_offset_top = header_dom.clientHeight + 50;
+
+    window.addEventListener("scroll", () => {
+        let scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scroll >= nav_offset_top) {
+            header_dom.classList.add("navbar-fixed");
+        } else {
+            header_dom.classList.remove("navbar-fixed");
+        }
+    });
+}
+
+function setMenuActive() {
+    const sections = document.querySelectorAll("section");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((li) => {
+            li.classList.remove("active");
+
+            if (current == li.getAttribute("href").split("#")[1]) {
+                li.classList.add("active");
+            }
+        });
+    });
+}
+
+function onMenuClick() {
+    console.log(navLinks);
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", changeMenuState);
+    };
+
+}
+
+function onMenuClickBtn() {
+
+    navLinkBtn.addEventListener("click", changeMenuState);
+}
+
+navbarFixed();
+setMenuActive();
+onMenuClick();
+onMenuClickBtn();
